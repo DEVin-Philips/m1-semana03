4f12b7d4d02eff7cb6849b99b4f5084e2c911aaa
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-10-17 20:06:37 -0300

    adiciona exemplos iniciais m1-s03-a1

---
+++ aula1/app.js
@@ -0,0 +1,36 @@
+console.log('Olá! m1-s03-a1')
+
+// nome comum de variável
+var nomeComumDeVariavel = 'yey'
+
+// nome de entidade classe/componente
+var EntidadeEmVariavel = {}
+
+// nome de dado constante predefinido
+var TAMANHO_DO_ARRAY = 3
+
+// console.log(typeof 'romeu') // string
+// console.log(typeof 334.4) // number
+// console.log(typeof true) // boolean
+// console.log(typeof undefined) // undefined
+// console.log(typeof null) // object
+// console.log(typeof []) // object
+// console.log(typeof {}) // object
+// console.log(typeof NaN) // number
+// console.log(typeof function () {}) // function
+
+// valores equivalentes a falso
+// console.log(!!false)
+// console.log(!!0)
+// console.log(!!'')
+// console.log(!!null)
+// console.log(!!undefined)
+// console.log(!!NaN)
+
+// valores equivalentes a true
+// console.log(!!true)
+// console.log(!!3232)
+// console.log(!!'asdfasd')
+// console.log(!![])
+// console.log(!!{})
+// console.log(!!function () {})
