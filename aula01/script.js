ee9fbf0f7c06aaddf9e853e8d7a16c78e20fe6e7
Gustavo Maia <gustavojv_maia@outlook.com>
2022-10-17 20:09:48 -0300

    Aula01 semana 03-m01
    Primeira parte da aula 01

---
+++ aula01/script.js
@@ -0,0 +1,43 @@
+alert('Olá, Mundo!');
+
+// nome comum para variáveis
+var nomeComumParaVariavel = 'camelCase';
+
+// nomeando entidades, iniciamos com letra maiúscula
+var EntidadeEmVariavel = {};
+
+// nome para um dado/valor constante/predefinido
+// tudo maiusculo, separado por underline
+var TAMANHO_DO_ARRAY = 3;
+
+/*
+console.log(typeof 'Olá, Mundo'); // string
+console.log(typeof 1983); // numero, independente se é float ou int
+console.log(typeof true); // booleano
+console.log(typeof undefined); // tipo não definido
+console.log(typeof null); // o typooff vê o null como um objeto
+console.log(typeof {}); // visto como objeto
+console.log(typeof []); // abertura e fechamento de colchetes, são arrays, mas vistos como um objeto
+console.log(typeof NaN); // o valor "not a number" é reconhecido como um número
+console.log(typeof function {}); //
+*/
+
+// valores equivalentes a falso
+// console.log(!!false)
+// console.log(!!0)
+// console.log(!!'')
+// console.log(!!null)
+// console.log(!!undefined)
+// console.log(!!NaN)
+
+// valores equivalentes a true
+// console.log(!!true)
+// console.log(!!3232)
+// console.log(!!'asdfasd')
+// console.log(!![])
+// console.log(!!{})
+// console.log(!!function () {})
+
+var exemplo = 4;
+exemplo++
+console.log(exemplo);
