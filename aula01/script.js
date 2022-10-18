bbff6c04cb5fd9c61a28cf85d67aeae0e72a8c91
Gustavo Maia <gustavojv_maia@outlook.com>
2022-10-17 22:04:38 -0300

    Final da aula 01

--- aula01/script.js
+++ aula01/script.js
@@ -41,3 +41,57 @@ console.log(typeof function {}); //
 var exemplo = 4;
 exemplo++
 console.log(exemplo);
+
+
+// inicializa array
+var vetor = ['3', 'str', '#', 'true']
+
+// console.log(vetor)
+// console.log(vetor[1])
+// // atributo importante length
+// console.log(vetor.length)
+
+// vetor.push(999)
+// vetor.push(111)
+vetor.push(555, 444, 333)
+
+// como testar se é um array
+// if (Array.isArray(vetor)) {
+//   for (var i = 0; i < vetor.length; i++) {
+//     console.log(vetor[i])
+//   }
+// } else {
+//   console.log(vetor)
+// }
+
+//console.log(vetor.join(' '))
+
+const pessoas = [
+  {
+    nome: {
+      primeiro: 'romeu',
+      ultimo: 'casarotto',
+    },
+    idade: 30,
+    ocupacoes: ['programador'],
+  },
+  {
+    nome: 'isaac',
+    idade: 30,
+    ocupacoes: ['professor'],
+  },
+]
+
+console.log(pessoas[0].nome.primeiro)
+
+
+function multiplica(x, y) {
+    let acumulador = 0
+    for (let i = 0; i < y; i++) {
+      acumulador += x
+    }
+    return acumulador
+  }
+  
+  console.log(multiplica(3, 8))
+  
