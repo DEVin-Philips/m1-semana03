ab70ceb1fe00bbff5eff55a5a54c342f0400456e
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-10-21 21:32:35 -0300

    adiciona exercicio7 m1-s03

--- exercicios/e7/app.js
+++ exercicios/e7/app.js
@@ -1,1 +1,19 @@
 console.log('m1-s03-e7');
+
+function montaMensagem(pessoa) {
+  const { nome, idade, profissao } = pessoa;
+  return `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+}
+
+const montaArrow = ({ nome, idade, profissao }) =>
+ `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+
+const pessoa = {
+  nome: 'Ada',
+  idade: 36,
+  profissao: 'matemática'
+};
+
+const msg = montaArrow(pessoa);
+
+console.log(msg);
