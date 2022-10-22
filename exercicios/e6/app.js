2b481ea9e9b8f5751252341c9f6c121f384a0200
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-10-21 21:21:20 -0300

    adiciona exercicio6 m1-s03

--- exercicios/e6/app.js
+++ exercicios/e6/app.js
@@ -1,1 +1,36 @@
 console.log('m1-s03-e6');
+
+function verificaPalindromo(texto) {
+  if (typeof texto !== 'string') {
+    console.log('Texto inválido!');
+    return false; // interrompe a execuçao da funcao
+  }
+
+  const meio = texto.length / 2;
+  const fim = texto.length - 1;
+ 
+  for (let i = 0; i < meio; i++) {
+    if (texto[i] !== texto[fim - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// versao arrow com reverse
+const verificaPalindromoB = texto => {
+  if (typeof texto !== 'string') {
+    console.log('Texto inválido!');
+    return false; // interrompe a execuçao da funcao
+  }
+  return [...texto].reverse().join('') === texto;
+}
+
+const teste1 = verificaPalindromoB("ana");
+console.log('ana', teste1);
+// true
+
+const teste2 = verificaPalindromoB("julia");
+console.log('julia', teste2);
+// false
