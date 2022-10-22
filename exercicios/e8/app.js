c7b3b0da203e460716979e97dcfda0bb02f4ed27
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-10-21 22:03:05 -0300

    adiciona exemplo extra e1 m1-s03

--- exercicios/e8/app.js
+++ exercicios/e8/app.js
@@ -18,7 +18,23 @@ const r1 = uneDobraRetorna([1, 2, 3], 4, 4)
 //console.log(r1) // [1, 2, 3, 8, 8]
 
 const r2 = uneDobraRetornaMap([2], 10, 4, 8) 
-console.log(r2) // [2, 20, 8, 16]
+//console.log(r2) // [2, 20, 8, 16]
 
 const r3 = uneDobraRetorna([6, 8]) 
 //console.log(r3) // [6, 8]
+
+
+
+// EXEMPLO MAP
+const numeros = [3, 6, 2, 8];
+
+numeros.forEach((n) => {
+  //console.log({ n, i, a })
+});
+
+const resultado = numeros.map((num, idx) => {
+  //console.log({ n, i, a })
+  return `${num} - ${idx}`;
+});
+
+//console.log(resultado);
